b1ea866d67540be8848320aff1cad3d0b4c06ad6
ks.krasssss <self.developmentkkras@gmail.com>
2026-09-21 01:50:25 +0300

    refactor: re-export public API

---
+++ src/trips/__init__.py
@@ -0,0 +1,14 @@
+from .calculator import Add, Div, Expr, Mul, Neg, Num, Pow, Sqrt, Sub, Sum, evaluate
+from .hof import compose, curry3, make_predicate, make_running_total, pipe
+from .lazy import chunked, drop, g_keep, g_normalize, g_parse, record_stream, take, take_while
+from .model import Record, add_km, to_record, with_to_zone
+from .pipeline import Result, aggregate, keep, normalize, parse, process, run_comprehension, run_functional
+
+__all__ = [
+    "Add", "Div", "Expr", "Mul", "Neg", "Num", "Pow", "Sqrt", "Sub", "Sum", "evaluate",
+    "compose", "curry3", "make_predicate", "make_running_total", "pipe",
+    "chunked", "drop", "g_keep", "g_normalize", "g_parse", "record_stream", "take", "take_while",
+    "Record", "add_km", "to_record", "with_to_zone",
+    "Result", "aggregate", "keep", "normalize", "parse", "process",
+    "run_comprehension", "run_functional",
+]
